cb587b51856c3a7a44823974adee8c76c398cfae
the_Shark <xuannhak13@gmail.com>
2015-11-29 15:28:19 +0700

    dang nhap-home

---
+++ KhuyenNong/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KhuyenNong.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+
+        public ActionResult Log_in()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+    }
+}
