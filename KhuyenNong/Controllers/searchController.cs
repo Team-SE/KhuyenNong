8d6865b65756facc4d4a7aea18b6d9ec76f45365
DucHuong <iamsonofgod95@gmail.com>
2015-11-29 21:32:41 +0700

    Nothing to change

--- KhuyenNong/Controllers/searchController.cs
+++ KhuyenNong/Controllers/searchController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using KhuyenNong.Models;
+using System.IO;
 
 namespace KhuyenNong.Controllers
 {
@@ -16,16 +17,21 @@ namespace KhuyenNong.Controllers
         {
             return View();
         }
-        public ActionResult searchResult(string keys, string tblname)
+        public ActionResult searchResult(string keys)
         {
+            
+           // String l = "~/database/knowledge/1.txt";
             using (Database1Entities db = new Database1Entities())
             {
-                var tble = db.KNOWLEDGES.Where(a => System.IO.File.ReadAllText(a.Contains).Contains(keys));
+               // var tble = db.KNOWLEDGES.Where(a => System.IO.File.ReadAllText(a.Contains).Contains(keys));
                 var tbleKnowLedge = (from a in db.KNOWLEDGES
-                                     where true
-                                     select new resultSearch { Author = a.Author, Title = a.Title, Contains = System.IO.File.ReadAllText(a.Contains), DateWritten = "", Id = a.Id.ToString()}).ToList();
-                var listRes = tbleKnowLedge.Where(x => x.Contains.Contains(keys)).ToList();
-                return View(listRes);
+                                     join b in db.User_Table on a.Author equals b.Email
+                                     select new rowSearch { Author = b.username, Title = a.Title, Contains = a.Contains, DateWritten = "", Id = a.Id.ToString()}).ToList();
+                var mapping = tbleKnowLedge.Select(a => new rowSearch { Author = a.Author, Title = a.Title, Contains = System.IO.File.ReadAllText(Server.MapPath(a.Contains)), DateWritten = "", Id = a.Id.ToString() }).ToList();
+                var listRes = mapping.Where(x => x.Contains.Contains(keys)).ToList();
+
+                //var s = System.IO.Directory.GetCurrentDirectory();
+                return View(new searchResult(listRes, keys));
             }
         }
 
---
+++ KhuyenNong/Models/rowSearch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KhuyenNong.Models
+{
+    public class rowSearch
+    {
+        public String Title {get; set;}
+        public String Contains{get; set;}
+        public String Author{get; set;}
+        public String DateWritten{get; set;}
+        public String Id{get; set;}
+        public rowSearch(string tt, string ct, string au, string d, string id)
+        {
+            this.Title = tt;
+            this.Contains = ct;
+            this.Author = au;
+            this.DateWritten = d;
+            this.Id = id;
+        }
+        public rowSearch() { }
+    }
+}
